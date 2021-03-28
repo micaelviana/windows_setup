fd1fc6152ee4dc47ef1e6797085eb82b31ce48a1
Micael Viana <44692237+micaelviana@users.noreply.github.com>
2021-03-27 20:26:32 -0400

    Update settings.ps1

--- settings.ps1
+++ settings.ps1
@@ -1,4 +1,9 @@
+#Modules
 Install-Module posh-git -Scope CurrentUser
 Install-Module oh-my-posh -Scope CurrentUser
 Install-Module PSReadLine -Scope CurrentUser
+
+#Neovim
+iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
+    ni "$(@($env:XDG_DATA_HOME, $env:LOCALAPPDATA)[$null -eq $env:XDG_DATA_HOME])/nvim-data/site/autoload/plug.vim" -Force
 npm install -g neovim
