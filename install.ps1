059609093d61c15d79fb93feb6f65da6d5f1e7d3
Micael Viana <44692237+micaelviana@users.noreply.github.com>
2021-04-29 00:09:31 -0400

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,19 +1,20 @@
 # powershell -executionpolicy bypass -File C:\Users\mypath\install.ps1 
-# apps that have context menu that i domt want: sublime,vscode,vlc
+# apps that have context menu that i domt want: sublime,vscode,vlc, format factory
+#better version of Kate Editor is from Microsoft Store
 $Packages = '7zip',
-            'adobereader',
-            'ccleaner',
-            'driverbooster',
-            'formatfactory',
-            'git',
-            'googlechrome',
-            'make',
-            'microsoft-windows-terminal',
-            'neovim',
-            'nodejs.install',
-            'qbittorrent',
-            'virtualbox',
-            'wps-office-free'
+    'adobereader',
+    'ccleaner',
+    'driverbooster',
+    'formatfactory',
+    'git',
+    'googlechrome',
+    'make',
+    'microsoft-windows-terminal',
+    'neovim',
+    'nodejs.install',
+    'qbittorrent',
+    'virtualbox',
+    'wps-office-free'
             
 
   # InstallChoco
