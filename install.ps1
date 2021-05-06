ea245e6221f0227abc77657e63fcb492accfa34b
micaelviana <micael.vianasilva@gmail.com>
2021-05-06 00:13:21 -0400

    test graphical git

--- install.ps1
+++ install.ps1
@@ -1,6 +1,7 @@
 # powershell -executionpolicy bypass -File C:\Users\mypath\install.ps1 
 # apps that have context menu that i domt want: sublime,vscode,vlc, format factory, git
 #better version of Kate Editor is from Microsoft Store
+#comment 
 $Packages = '7zip',
     'adobereader',
     'ccleaner',
