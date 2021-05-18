540c47badd05a28a698a8daf2dbcf6eaa058b6d8
micaelviana <micael.vianasilva@gmail.com>
2021-05-18 12:07:55 -0400

    script without alias

--- settings.ps1
+++ settings.ps1
@@ -7,7 +7,7 @@ Install-Module PSReadLine -Scope CurrentUser
 
 git clone https://github.com/micaelviana/nvim $env:USERPROFILE\AppData\Local\nvim
 
-iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
-    ni "$(@($env:XDG_DATA_HOME, $env:LOCALAPPDATA)[$null -eq $env:XDG_DATA_HOME])/nvim-data/site/autoload/plug.vim" -Force
+Invoke-WebRequest -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
+    New-Item "$(@($env:XDG_DATA_HOME, $env:LOCALAPPDATA)[$null -eq $env:XDG_DATA_HOME])/nvim-data/site/autoload/plug.vim" -Force
 
 npm install -g neovim
