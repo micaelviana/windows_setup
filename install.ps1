889109199dc7af13e1d8c93d5f4c0715cbdc1f25
Micael Viana <44692237+micaelviana@users.noreply.github.com>
2021-05-20 12:21:38 -0400

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,23 +1,22 @@
 # powershell -executionpolicy bypass -File C:\Users\mypath\install.ps1 
 # apps that have context menu that i domt want: format factory,git,vscode,vlc, 
 $Packages = '7zip',
-    'adobereader',
-    'ccleaner',
-    'driverbooster',
-    'formatfactory',
-    'googlechrome',
-    'make',
-    'microsoft-windows-terminal',
-    'neovim',
-    'nodejs.install',
-    'qbittorrent',
-    'virtualbox',
-    'wps-office-free'
+'adobereader',
+'ccleaner',
+'driverbooster',
+'formatfactory',
+'googlechrome',
+'make',
+'microsoft-windows-terminal',
+'neovim',
+'nodejs.install',
+'qbittorrent',
+'virtualbox',
+'wps-office-free'
             
 
   # InstallChoco
   # DoYourPackageInstallStuff
-  ForEach ($PackageName in $Packages)
-    {
-        choco install $PackageName -y
-    }
+ForEach ($PackageName in $Packages){
+	choco install $PackageName -y
+}
