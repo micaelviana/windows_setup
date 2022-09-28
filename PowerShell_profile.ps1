4078d83e5af7a9f17bc5f16798ea0d8b4aa19b49
Micael Viana <44692237+micaelviana@users.noreply.github.com>
2022-09-27 21:32:06 -0400

    Update PowerShell_profile.ps1

--- PowerShell_profile.ps1
+++ PowerShell_profile.ps1
@@ -1,6 +1,6 @@
 #Prompt
 # oh-my-posh init pwsh | Invoke-Expression 
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\hunk.omp.json" | Invoke-Expression 
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\1_shell.omp.json" | Invoke-Expression 
 #Modules
 # PSReadLine
 Set-PSReadLineOption -EditMode Windows
@@ -13,37 +13,48 @@ Import-Module PSFzf
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
 Set-Alias lvim C:\Users\Micael\.local\bin\lvim.ps1
 
+#Zoxide
+# For zoxide v0.8.0+
+Invoke-Expression (& {
+    $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
+    (zoxide init --hook $hook powershell | Out-String)
+})
+
+
+#LSD
+Set-Alias ls lsd
+function ll{lsd -l}
+function la{lsd -a}
+function lla{lsd -la}
 
 #Aliases
-Set-alias l ls
-Set-alias ll ls
 Set-alias vi nvim
 Set-alias g git
 Set-alias clipcopy clip.exe
 
-function which ($command) {
+function which ($command){
   Get-Command -Name $command -ErrorAction SilentlyContinue |
     Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
 }
 
 
 #Remove-Item doesn't delete folders by default, -Force solve the problem
-function remove {
-    ForEach ($filename in $args){
-        Remove-Item -Force $filename
-    }
+function remove{
+  ForEach ($filename in $args){
+    Remove-Item -Force $filename
+  }
 }
 
 #just works in adm mode
-function link ($target, $link) {
-    New-Item -Path $link -ItemType SymbolicLink -Value $target
+function link ($target, $link){
+  New-Item -Path $link -ItemType SymbolicLink -Value $target
 }
 
 function copyfile($file){
-    cat $file | clip.exe
+  Get-content $file | clip.exe
 }
 
 #alias ..=cd .., just like in zsh
-function .. {
-    Set-Location ..
+function ..{
+  Set-Location ..
 }
