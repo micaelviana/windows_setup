76104b9c9e66fbb0c912da1bb658ddb7d53c5728
Micael Viana <44692237+micaelviana@users.noreply.github.com>
2022-09-28 15:20:12 -0400

    Update PowerShell_profile.ps1

--- PowerShell_profile.ps1
+++ PowerShell_profile.ps1
@@ -1,11 +1,13 @@
 #Prompt
 # oh-my-posh init pwsh | Invoke-Expression 
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\1_shell.omp.json" | Invoke-Expression 
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\gruvbox.omp.json" | Invoke-Expression 
 #Modules
 # PSReadLine
-Set-PSReadLineOption -EditMode Windows
-Set-PSReadLineOption -BellStyle None
 Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -BellStyle None
+
+#Terminal-Icons
 Import-Module -Name Terminal-Icons
 # Fzf
 Import-Module PSFzf
@@ -36,6 +38,20 @@ Set-alias vi nvim
 Set-alias g git
 Set-alias clipcopy clip.exe
 
+#aliases to git binaries
+Set-alias mkdir C:\Users\Micael\scoop\apps\git\current\usr\bin\mkdir.exe
+Set-alias cat C:\Users\Micael\scoop\apps\git\current\usr\bin\cat.exe
+Set-alias less C:\Users\Micael\scoop\apps\git\current\usr\bin\less.exe
+Set-alias head C:\Users\Micael\scoop\apps\git\current\usr\bin\head.exe
+Set-alias tail C:\Users\Micael\scoop\apps\git\current\usr\bin\tail.exe
+Set-alias grep C:\Users\Micael\scoop\apps\git\current\usr\bin\grep.exe
+Set-alias sed C:\Users\Micael\scoop\apps\git\current\usr\bin\sed.exe
+Set-alias awk C:\Users\Micael\scoop\apps\git\current\usr\bin\gawk.exe
+Set-alias touch C:\Users\Micael\scoop\apps\git\current\usr\bin\touch.exe
+Set-alias find C:\Users\Micael\scoop\apps\git\current\usr\bin\find.exe
+Set-alias rm C:\Users\Micael\scoop\apps\git\current\usr\bin\rm.exe
+Set-alias rmdir C:\Users\Micael\scoop\apps\git\current\usr\bin\rmdir.exe
+
 #{{Functions}}
 function which ($command){
   Get-Command -Name $command -ErrorAction SilentlyContinue |
@@ -64,7 +80,11 @@ function ..{
   Set-Location ..
 }
 
-#winget alias
+#winget install from winget
 function winstall($package){
     winget install $package --source winget
-  }
+}
+#winget install from store
+function msinstall($package){
+    winget install $package --source msstore
+}
