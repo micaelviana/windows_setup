04b30cd03b07ec62c93c9542399feaf14515e810
Micael Viana <44692237+micaelviana@users.noreply.github.com>
2022-12-25 23:05:04 -0400

    Create scoop.ps1

---
+++ scoop.ps1
@@ -0,0 +1,22 @@
+##PowerShell.exe -File <FileName> -ExecutionPolicy Bypass
+scoop install aria2
+scoop install bat
+scoop install curl
+scoop install fd
+scoop install fnm
+scoop install fzf
+scoop install gawk
+scoop install lsd
+scoop install ripgrep
+scoop install sudo
+scoop install telnet
+scoop install time
+scoop install tldr
+scoop install tree-sitter
+scoop install wget
+scoop install zip
+scoop install zoxide
+scoop install sort-uniq-wc
+scoop install psfzf
+scoop install psreadline
+scoop install vlc
