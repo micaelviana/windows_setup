9a0fc289eb163a7ab2d18d41fa6eb08d1278164a
Micael Viana <44692237+micaelviana@users.noreply.github.com>
2023-01-27 18:33:04 -0400

    Update scoop.ps1

--- scoop.ps1
+++ scoop.ps1
@@ -1,18 +1,13 @@
 ##PowerShell.exe -File <FileName> -ExecutionPolicy Bypass
+scoop bucket add extras
 scoop install aria2
 scoop install bat
 scoop install curl
-scoop install fd
-scoop install fnm
 scoop install fzf
 scoop install gawk
 scoop install lsd
-scoop install ripgrep
-scoop install sudo
-scoop install telnet
-scoop install time
+scoop install nodejs
 scoop install tldr
-scoop install tree-sitter
 scoop install wget
 scoop install zip
 scoop install zoxide
