028f1064731aad1044318cf72c708249a6b01c9f
Micael Viana <44692237+micaelviana@users.noreply.github.com>
2023-08-24 22:49:26 -0400

    add choco packages

---
+++ chocolatey.ps1
@@ -0,0 +1,17 @@
+# PowerShell.exe -File <FileName> -ExecutionPolicy Bypass 
+choco install 7zip
+choco install ditto
+choco install flameshot
+choco install googlechrome
+choco install mactype
+choco install microsoft-teams
+choco install microsoft-windows-terminal
+choco install modernflyouts
+choco install neovim
+choco install notepadplusplus
+choco install notion
+choco install okular
+choco install powershell
+choco install powertoys
+choco install telegram
+choco install vscode
