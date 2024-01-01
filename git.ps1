3e8be6533588ad0c67b3e92f0a36184651e37a8b
Micael Viana <micael.vianasilva@gmail.com>
2024-01-01 12:45:26 -0700

    new file

---
+++ git.ps1
@@ -0,0 +1,5 @@
+git config --global user.name "Micael Viana"
+git config --global user.email "micael.vianasilva@gmail.com"
+git config --global core.editor nvim
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=604800'
