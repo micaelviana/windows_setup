0fa454ce76d83c0647ec4cde273166e266f917cc
Micael Viana <micael.vianasilva@gmail.com>
2024-04-14 11:19:45 -0700

    update

--- winget.ps1
+++ winget.ps1
@@ -1,3 +1,4 @@
+winget install --id=9NKSQGP7F2NH -e #whatsapp
 winget install --id=Bopsoft.Listary -e  
 winget install --id=Ditto.Ditto -e  
 winget install --id=Espanso.Espanso -e  
@@ -6,6 +7,7 @@ winget install --id=Git.Git -e
 winget install --id=Google.Chrome -e  
 winget install --id=KDE.Okular -e  
 winget install --id=LocalSend.LocalSend -e  
+winget install --id=MacType.MacType -e
 winget install --id=Microsoft.VisualStudioCode -e 
 winget install --id=Notepad++.Notepad++ -e  
 winget install --id=Notion.Notion -e  
@@ -14,5 +16,3 @@ winget install --id=SoftwareOK.Q-Dir -e
 winget install --id=Telegram.TelegramDesktop -e  
 winget install --id=qBittorrent.qBittorrent -e  
 winget install --id=voidtools.Everything -e  
-winget install --id=9NKSQGP7F2NH -e #whatsapp
-
