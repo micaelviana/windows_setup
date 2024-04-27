1f9c9daa214f8996203cf120396a7cfd84e08e47
Micael Viana <micael.vianasilva@gmail.com>
2024-04-27 13:31:20 -0700

    update

--- scoop.ps1
+++ scoop.ps1
@@ -6,6 +6,7 @@ scoop install https://raw.githubusercontent.com/lukesampson/shimexe/master/scoop
 scoop install lsd
 scoop install make
 scoop install mingw
+scoop install nerd-fonts/CodeNewRoman-NF-Mono
 scoop install nerd-fonts/Hack-NF-Mono
 scoop install nodejs
 scoop install psfzf
