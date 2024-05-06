35b65368471bdbd0bc9d7d916990258505e2f8d2
unknown <micael.vianasilva@gmail.com>
2024-05-06 15:41:25 -0400

    update

--- winget.ps1
+++ winget.ps1
@@ -1,18 +1,12 @@
 winget install --id=9NKSQGP7F2NH -e #whatsapp
 winget install --id=Bopsoft.Listary -e  
 winget install --id=Ditto.Ditto -e  
-winget install Neovim.Neovim
-winget install -e --id Python.Python.3.10
-winget install Microsoft.PowerToys --source winget
-winget install Microsoft.PowerShell --source winget
 winget install --id=Espanso.Espanso -e  
 winget install --id=Flameshot.Flameshot -e  
 winget install --id=Git.Git -e  
 winget install --id=Google.Chrome -e  
 winget install --id=KDE.Okular -e  
 winget install --id=LocalSend.LocalSend -e  
-winget install -e --id Mozilla.Firefox.ESR
-winget install -e --id wez.wezterm
 winget install --id=MacType.MacType -e
 winget install --id=Microsoft.VisualStudioCode -e 
 winget install --id=Notepad++.Notepad++ -e  
@@ -22,3 +16,10 @@ winget install --id=SoftwareOK.Q-Dir -e
 winget install --id=Telegram.TelegramDesktop -e  
 winget install --id=qBittorrent.qBittorrent -e  
 winget install --id=voidtools.Everything -e  
+winget install -e --id Mozilla.Firefox.ESR
+winget install -e --id Python.Python.3.10
+winget install -e --id Upscayl.Upscayl
+winget install -e --id wez.wezterm
+winget install Microsoft.PowerShell --source winget
+winget install Microsoft.PowerToys --source winget
+winget install Neovim.Neovim
