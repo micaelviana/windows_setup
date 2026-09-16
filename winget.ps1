75bdffae8de6dde87b1c13348c2b2a20dc51444b
Micael Viana <44692237+micaelviana@users.noreply.github.com.>
2026-09-16 15:26:22 -0400

    update winget list

--- winget.ps1
+++ winget.ps1
@@ -1,23 +1,17 @@
 winget install  --id AdrienAllard.FileConverter
 winget install  --id Mozilla.Firefox.ESR -e
 winget install  --id Python.Python.3.10 -e
-winget install  --id Upscayl.Upscayl -e
 winget install  --id wez.wezterm -e
-winget install --id 9NKSQGP7F2NH -e #whatsapp
-winget install --id Bopsoft.Listary -e  
 winget install --id Ditto.Ditto -e  
 winget install --id Espanso.Espanso -e  
-winget install --id Flameshot.Flameshot -e  
 winget install --id Git.Git -e  
 winget install --id Google.Chrome -e  
 winget install --id KDE.Okular -e  
 winget install --id LocalSend.LocalSend -e  
-winget install --id M2Team.NanaZip
 winget install --id Microsoft.VisualStudioCode -e 
 winget install --id Neovim.Neovim
 winget install --id Notepad++.Notepad++ -e  
 winget install --id Notion.Notion -e  
-winget install --id SoftDeluxe.FreeDownloadManager -e  
 winget install --id SoftwareOK.Q-Dir -e  
 winget install --id Telegram.TelegramDesktop -e  
 winget install --id qBittorrent.qBittorrent -e  
